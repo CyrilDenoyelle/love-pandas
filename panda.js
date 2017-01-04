797f10abba6d405969db844db764973bb045d3fb
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-01-05 00:34:18 +0100

    j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda j'aime les panda

--- panda.js
+++ panda.js
@@ -4,22 +4,10 @@ $('.panda').click(function() {
 	var type = $(this).attr("data-type");
 	var tendance = $(this).attr("data-tendance");
 
-    if (type == "normaux" && tendance == "amour") {// Aime normaux
-    	$(".output2").toggle();
-    	$(".output3").toggle();
-    }
-	else if (type == "normaux" && tendance == "haine") {// Aime pas normaux
-		$('.output2').toggle();
-		$('.output4').toggle()
-	}
-	else if (type == "roux" && tendance == "amour") {// Aime roux
-		$('.output1').toggle();
-		$('.output3').toggle();
-	}
-	else if (type == "roux" && tendance == "haine") {// Aime pas roux
-		$('.output1').toggle();
-		$('.output4').toggle()
+	if (type != "undefined" ) {
+
+		$('#recever').html(type + " " + tendance);
 	} else {
-		$('#what').toggle();
+		$('#recever').html('mais de kowwwaaa ?');
 	}
 });
